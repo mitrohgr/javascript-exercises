6a436351961016c152d9f5b4987afebc0eb32121
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-17 15:18:11 +0530

    Solve "Arrays and Loops" exercises
    My solutions contain the following: use of variables and operators,
    data types and conditionals, functions, problem solving, etc.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const arr = string.split("");
+  let reversed = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed += arr[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+  let newArr;
+  for (let i = 0; i < numbers.length; i++) {
+    newArr = arr.filter((num) => !numbers.includes(num));
+  }
+  return newArr;
 };
 
 // Do not edit below this line
